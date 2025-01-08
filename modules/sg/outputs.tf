9cc3d44ebbc5333889dd4175e7cecb538035e61d
thoong2 <acc.thongnguyen98.84@gmail.com>
2025-01-08 01:32:10 +0000

    Update outputs.tf

--- modules/sg/outputs.tf
+++ modules/sg/outputs.tf
@@ -1,4 +1,4 @@
 output "security_group_ids" {
   description = "IDs of the created security groups"
-  value = { for sg_name, sg in aws_security_group.this : sg_name => sg.id }
+  value       = { for sg in aws_security_group.this : sg.key => sg.value.id }
 }
