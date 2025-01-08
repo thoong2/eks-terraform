37f0acbc97c902fe35923a33f683c50d38efc85b
thoong2 <acc.thongnguyen98.84@gmail.com>
2025-01-08 10:20:52 +0000

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,11 +1,20 @@
 output "cluster_role_arn" {
   description = "ARN of the EKS cluster IAM role"
-  value       = aws_iam_role.eks_cluster_role.arn
+  value       = aws_iam_role.eks_cluster.arn
 }
 
 output "node_group_role_arns" {
-  description = "Map of node group IAM role ARNs"
+  description = "ARNs of the node group IAM roles"
   value = {
-    for k, v in aws_iam_role.node_group_role : k => v.arn
+    for ng_key, ng_value in aws_iam_role.node_groups :
+    ng_key => ng_value.arn
+  }
+}
+
+output "node_group_role_names" {
+  description = "Names of the node group IAM roles"
+  value = {
+    for ng_key, ng_value in aws_iam_role.node_groups :
+    ng_key => ng_value.name
   }
 }
