a41435bdd0540b82765af19430a761c6cc08ee78
thoong2 <acc.thongnguyen98.84@gmail.com>
2025-01-08 01:30:55 +0000

    Update variables.tf

--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -2,19 +2,18 @@ variable "security_groups" {
   description = "Map of security groups to create"
   type = map(object({
     description = string
-    ingress = object({
+    ingress     = list(object({
       from_port   = number
       to_port     = number
       protocol    = string
       cidr_blocks = list(string)
-    })
-    egress = object({
+    }))
+    egress      = list(object({
       from_port   = number
       to_port     = number
       protocol    = string
       cidr_blocks = list(string)
-    })
-    tags = map(string)
+    }))
   }))
 }
 
