e54f62cf556fba214013fc205d5463794b0019aa
thoong2 <acc.thongnguyen98.84@gmail.com>
2025-01-08 10:21:17 +0000

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -3,8 +3,28 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "node_group_policies" {
-  description = "Map of node group names to their custom policy statements"
-  type        = map(list(any))
+variable "node_groups" {
+  description = "Map of node group configurations including IAM policies"
+  type = map(object({
+    node_group = object({
+      policies = list(string)
+    })
+  }))
+}
+
+variable "policies_path" {
+  description = "Path to the directory containing policy JSON files. Can be absolute or relative path"
+  type        = string
+  default     = "./modules/policies"
+  
+  validation {
+    condition     = can(regex("^[./]", var.policies_path))
+    error_message = "The policies_path must start with ./ for relative path or / for absolute path."
+  }
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
   default     = {}
 }
